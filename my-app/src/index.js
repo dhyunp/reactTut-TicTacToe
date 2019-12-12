831f5531fc7b47eca71510d91143b1c3c5afa5d2
dhyunpark <danpark0605@gmail.com>
2019-12-12 17:45:33 +0900

    up to showing past moves

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,19 +16,6 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   
-  handleClick(i){
-	const squares = this.state.squareStatus.slice();
-	if (calculateWinner(squares) || squares[i]) {
-		return;
-	}
-	
-	squares[i] = this.state.xIsNext? 'X' : 'O';
-	this.setState({
-		squareStatus: squares,
-		xIsNext: !this.state.xIsNext,
-		});
-  }
-  
   renderSquare(i){
     return (<Square 
 	value={this.props.squareStatus[i]}
@@ -37,17 +24,8 @@ class Board extends React.Component {
   }
 
   render() {
-	const winner = calculateWinner(this.state.squareStatus);
-	let status;
-	if(winner){
-		status = 'Winner: ' + winner;
-	} else {
-		status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
-	}
-
-    return (
+	return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -78,16 +56,47 @@ class Game extends React.Component {
 		xIsNext: true,
 	};
   }
+  
+  handleClick(i){
+	const history = this.state.history;
+	const current = history[history.length-1];
+	const squares = current.squareStatus.slice();
+	if (calculateWinner(squares) || squares[i]) {
+		return;
+	}
 	
+	squares[i] = this.state.xIsNext? 'X' : 'O';
+	this.setState({
+		history: history.concat([{
+		  squareStatus:squares,
+		}]),
+		xIsNext: !this.state.xIsNext,
+	});
+  }	
 
   render() {
+	  
+	const history = this.state.history;
+	const current = history[history.length-1];
+	const winner = calculateWinner(current.squareStatus);
+	
+	let status;
+	if (winner){
+		status = "Winner: " + winner;
+	}else{
+		status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+	}
+	
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+		    squareStatus={current.squareStatus}
+			onClick={(i) => this.handleClick(i)}
+			/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
